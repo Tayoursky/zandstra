9c67775c06d38984e83a646e669dfc3b5dec56f4
Boris Tayoursky <tayoursky@gmail.com>
2018-01-24 14:09:24 +0900

    add property in class

--- index.php
+++ index.php
@@ -1,12 +1,18 @@
 <?php 
 class ShopProduct
 {
-    //body class    
+    public $title             = "Стандартный товар";
+    public $producerMainName  = "Булгаков";
+    public $producerFirstName = "Михаил";
 }
 $product1 = new ShopProduct();
-$product2 = new ShopProduct();
+//$product2 = new ShopProduct();
+$product1->title             = "Собачье сердце"; 
+$product1->producerMainName  = "Булгаков";
+$product1->producerFirstName = "Михаил";
+$product1->price             = 5.99;
 
-
-var_dump($product1);
-var_dump($product2);
+print "Автор: {$product1->producerFirstName} "
+      ."{$product1->producerMainName}\n";
+//print $product2->title="Ревизор";
 ?>
