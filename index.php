9946a5f92d12aa4c4808d98a8b18d99a441be005
Boris Tayoursky <tayoursky@gmail.com>
2018-01-26 14:04:30 +0900

    add construct and other, api xml

--- index.php
+++ index.php
@@ -4,15 +4,54 @@ class ShopProduct
     public $title             = "Стандартный товар";
     public $producerMainName  = "Булгаков";
     public $producerFirstName = "Михаил";
+    public $price             = 5.99;
+    
+    function __construct($title, $producerMainName, $producerFirstName, $price)
+    {
+        $this->title             = $title;
+        $this->producerMainName  = $producerMainName;
+        $this->producerFirstName = $producerFirstName;
+        $this->price             = $price;
+    }
+    function getProducer()
+    {
+        return  "{$this->producerFirstName} "
+               ."{$this->producerMainName}\n";
+    }
 }
-$product1 = new ShopProduct();
-//$product2 = new ShopProduct();
-$product1->title             = "Собачье сердце"; 
-$product1->producerMainName  = "Булгаков";
-$product1->producerFirstName = "Михаил";
-$product1->price             = 5.99;
+$product1 = new ShopProduct("Собачье сердце", "Булгаков", "Михаил", 5.99);
 
-print "Автор: {$product1->producerFirstName} "
-      ."{$product1->producerMainName}\n";
-//print $product2->title="Ревизор";
+print "Автор: {$product1->getProducer()} <br> ";
+
+class AddressManager
+{
+    private $addresses = array("209.131.36.159", "74.125.19.106");
+    
+    function outputAddresses($resolve)
+    {
+        foreach ($this->addresses as $address)
+        {
+            print $address;
+            
+            if(is_string($resolve))
+            {
+                $resolve =
+                (preg_match("/false|no|off/i", $resolve))?
+                false:true;               
+            }
+            if($resolve)
+            {
+                print "---( ".gethostbyaddr($address)." )";
+            }                
+            print "\n";
+        }
+       
+    }
+}
+$settings = simplexml_load_file("settings.xml");
+$manager = new AddressManager();
+//var_dump($settings->resolvedomains);exit;
+$manager->outputAddresses((string)$settings->resolvedomains);
+
+
 ?>
